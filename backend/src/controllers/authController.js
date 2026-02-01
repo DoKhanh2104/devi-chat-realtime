1944b7fc8750aec42981930149ebc40f4653eb24
DoKhanh2104 <winwin020104@gmail.com>
2026-02-01 14:46:14 +0700

    feat: implement user sign in with jwt

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { Session } from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m";
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000;
 
 export const signUp = async (request, response) => {
   try {
@@ -28,5 +34,49 @@ export const signUp = async (request, response) => {
   } catch (error) {
     console.error("Failed to sign up", error);
     return response.status(500).json({ message: "Inter Server Error" });
+  }
+};
+
+export const signIn = async (request, response) => {
+  try {
+    const { username, password } = request.body;
+    if (!username || !password) {
+      return response
+        .status(401)
+        .json({ message: "Username, password is required" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return response.status(401).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.hashedPassword,
+    );
+    if (!isPasswordCorrect) {
+      return response.status(401).json({ message: "Password invalid" });
+    }
+
+    // Compare is true
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: ACCESS_TOKEN_TTL },
+    );
+
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+
+    return response.status(200).json(`User ${username} is signed in `);
+  } catch (error) {
+    console.error("Failed to sign in", error);
+    return response.status(500).json({ message: "Internal Server Error" });
   }
 };
---
+++ backend/src/models/Session.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+import User from "./User.js";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+    refreshToken: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  },
+);
+sessionSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+export const Session = mongoose.model("Session", sessionSchema);
